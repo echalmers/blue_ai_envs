a59b29259d9160637d17722519ff3d684d787e49
echalmers <echalmers@mtroyal.ca>
2022-12-17 16:44:32 -0700

    add reqs to setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="blue_ai_envs",
     version="0.0.1",
-    # install_requires=["gym==0.26.0", "pygame==2.1.0"],
+    install_requires=["minigrid", "imageio"],
 )
